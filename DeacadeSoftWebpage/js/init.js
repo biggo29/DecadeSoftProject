6fe8461d677185befe8be9570a214f84dd2c6dd7
Biggo <shoaibshahriar29@gmail.com>
2017-08-09 11:51:54 +0600

    First commit

---
+++ DeacadeSoftWebpage/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.slider').slider({});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
